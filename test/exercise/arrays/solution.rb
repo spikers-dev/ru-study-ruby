2523a7e459d1c334579a927238d8f3f3abddf17a
Aleksandr Shpynev <arch.shpynev@gmail.com>
2022-09-28 20:00:06 +0300

    update solution for arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,25 +1,34 @@
 module Exercise
   module Arrays
     class << self
-      def replace(array)
-        max_el = array.inject do |el, el_next|
-          el > el_next ? el : el_next
-        end
-        array.map { |el| el.negative? ? el : max_el }
+      def find_max(array)
+        el_max = nil
+        array.each { |el| el_max = el if el_max.nil? || el_max < el }
+        el_max
       end
 
-      def search(_array, _query)
-        mid = (_array.length - 1) / 2
-        return -1 if mid <= 0 && _array[mid] != _query
+      def replace(array)
+        el_max = find_max(array)
+        array.map { |el| el.negative? ? el : el_max }
+      end
 
-        return mid if _array[mid] == _query
+      def search(array, query)
+        search_helper(array, query, 0, array.length)
+      end
 
-        if _array[mid] < _query
-          next_iter = search(_array[mid + 1..], _query)
-          next_iter == -1 ? -1 : mid + next_iter + 1
-        else
-          search(_array[0..mid], _query)
-        end
+      def search_helper(array, query, min, max)
+        mid = get_mid(min, max)
+        return mid if array[mid] == query
+
+        return -1 if max <= min || mid.zero?
+
+        array[mid] < query ? search_helper(array, query, mid, max) : search_helper(array, query, min, mid - 1)
+      end
+
+      def get_mid(min, max)
+        return 0 if max == 1
+
+        ((max - min) / 2.0).ceil + min
       end
     end
   end
